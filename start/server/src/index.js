9463c1fd840cfaafd098c015edc70b37e76892df
Nick Lee <zhongwei0717@gmail.com>
2019-05-21 00:32:26 +0800

    Apollo-Document-Tutorial 1. Build a schema. (update apollo-server and apollo-server-testing to version 2.5.0)

---
+++ start/server/src/index.js
@@ -0,0 +1,7 @@
+const { ApolloServer } = require('apollo-server');
+const typeDefs = require('./schema.js');
+
+const server = new ApolloServer({ typeDefs });
+server.listen().then(({ url }) => {
+    console.log(`🚀 Server ready at ${url}`);
+});
